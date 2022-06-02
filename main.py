b5e2a124b798bc5a3e3fd9ec4f193bd9042a154b
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2022-06-02 10:15:00 -0300

    Mecanica de movimentação e base do projeto

--- main.py
+++ main.py
@@ -1,182 +1,152 @@
 """
-Sprite Bullets
+Move Sprite With Keyboard
 
-Simple program to show basic sprite usage.
+Simple program to show moving a sprite with the keyboard.
+The sprite_move_keyboard_better.py example is slightly better
+in how it works, but also slightly more complex.
 
 Artwork from https://kenney.nl
 
 If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.sprite_bullets_aimed
+python -m arcade.examples.sprite_move_keyboard
 """
 
-import random
 import arcade
-import math
-import os
 
-SPRITE_SCALING_PLAYER = 0.5
-SPRITE_SCALING_COIN = 0.2
-SPRITE_SCALING_LASER = 0.8
-COIN_COUNT = 50
+SPRITE_SCALING = 0.5
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Sprites and Bullets Aimed Example"
+SCREEN_TITLE = "MJogo Processamento de imagens"
 
-BULLET_SPEED = 5
+MOVEMENT_SPEED = 5
 
-window = None
+
+
+class Player(arcade.Sprite): #classe do player
+    def update(self):
+
+        """ Move the player """
+
+        # Move player.
+
+        # Remove these lines if physics engine is moving player.
+
+        self.center_x += self.change_x
+
+        self.center_y += self.change_y
+
+
+
+        # Check for out-of-bounds (nao sair da tela)
+
+        if self.left < 0:
+
+            self.left = 0
+
+        elif self.right > SCREEN_WIDTH - 1:
+
+            self.right = SCREEN_WIDTH - 1
+
+
+
+        if self.bottom < 0:
+
+            self.bottom = 0
+
+        elif self.top > SCREEN_HEIGHT - 1:
+
+            self.top = SCREEN_HEIGHT - 1
+
 
 
 class MyGame(arcade.Window):
-    """ Main application class. """
+    """
+    Main application class.
+    """
 
-    def __init__(self):
-        """ Initializer """
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-        # Set the working directory (where we expect to find files) to the same
-        # directory this .py file is in. You can leave this out of your own
-        # code, but it is needed to easily run the examples using "python -m"
-        # as mentioned at the top of this program.
-        file_path = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(file_path)
+        super().__init__(width, height, title)
 
         # Variables that will hold sprite lists
         self.player_list = None
-        self.coin_list = None
-        self.bullet_list = None
 
         # Set up the player info
         self.player_sprite = None
-        self.score = 0
-        self.score_text = None
-
-        # Load sounds. Sounds from kenney.nl
-        self.gun_sound = arcade.sound.load_sound(":resources:sounds/laser1.wav")
-        self.hit_sound = arcade.sound.load_sound(":resources:sounds/phaseJump1.wav")
-
-        arcade.set_background_color(arcade.color.AMAZON)
-
-    def setup(self):
-
-        """ Set up the game and initialize the variables. """
-
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
-        self.bullet_list = arcade.SpriteList()
-
-        # Set up the player
-        self.score = 0
-
-        # Image from kenney.nl
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/"
-                                           "femalePerson_idle.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 70
-        self.player_list.append(self.player_sprite)
-
-        # Create the coins
-        for i in range(COIN_COUNT):
-
-            # Create the coin instance
-            # Coin image from kenney.nl
-            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
-
-            # Position the coin
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(120, SCREEN_HEIGHT)
-
-            # Add the coin to the lists
-            self.coin_list.append(coin)
 
         # Set the background color
         arcade.set_background_color(arcade.color.AMAZON)
 
-    def on_draw(self):
-        """ Render the screen. """
+    def setup(self):
+        """ Set up the game and initialize the variables. """
 
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player(":resources:images/animated_characters/female_person/"
+                                    "femalePerson_idle.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
         # This command has to happen before we start drawing
         self.clear()
-
         # Draw all the sprites.
-        self.coin_list.draw()
-        self.bullet_list.draw()
         self.player_list.draw()
-
-        # Put the text on the screen.
-        output = f"Score: {self.score}"
-        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        """ Called whenever the mouse button is clicked. """
-
-        # Create a bullet
-        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
-
-        # Position the bullet at the player's current location
-        start_x = self.player_sprite.center_x
-        start_y = self.player_sprite.center_y
-        bullet.center_x = start_x
-        bullet.center_y = start_y
-
-        # Get from the mouse the destination location for the bullet
-        # IMPORTANT! If you have a scrolling screen, you will also need
-        # to add in self.view_bottom and self.view_left.
-        dest_x = x
-        dest_y = y
-
-        # Do math to calculate how to get the bullet to the destination.
-        # Calculation the angle in radians between the start points
-        # and end points. This is the angle the bullet will travel.
-        x_diff = dest_x - start_x
-        y_diff = dest_y - start_y
-        angle = math.atan2(y_diff, x_diff)
-
-        # Angle the bullet sprite so it doesn't look like it is flying
-        # sideways.
-        bullet.angle = math.degrees(angle)
-        print(f"Bullet angle: {bullet.angle:.2f}")
-
-        # Taking into account the angle, calculate our change_x
-        # and change_y. Velocity is how fast the bullet travels.
-        bullet.change_x = math.cos(angle) * BULLET_SPEED
-        bullet.change_y = math.sin(angle) * BULLET_SPEED
-
-        # Add the bullet to the appropriate lists
-        self.bullet_list.append(bullet)
 
     def on_update(self, delta_time):
         """ Movement and game logic """
+        # Move the player
+        self.player_list.update()
 
-        # Call update on all sprites
-        self.bullet_list.update()
+    def on_key_press(self, key, modifiers):
 
-        # Loop through each bullet
-        for bullet in self.bullet_list:
+        """Called whenever a key is pressed. """
 
-            # Check this bullet to see if it hit a coin
-            hit_list = arcade.check_for_collision_with_list(bullet, self.coin_list)
+        # If the player presses a key, update the speed
+        if key == arcade.key.W:
+            self.player_sprite.change_y = MOVEMENT_SPEED
 
-            # If it did, get rid of the bullet
-            if len(hit_list) > 0:
-                bullet.remove_from_sprite_lists()
+        elif key == arcade.key.S:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
 
-            # For every coin we hit, add to the score and remove the coin
-            for coin in hit_list:
-                coin.remove_from_sprite_lists()
-                self.score += 1
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
 
-            # If the bullet flies off-screen, remove it.
-            if bullet.bottom > self.width or bullet.top < 0 or bullet.right < 0 or bullet.left > self.width:
-                bullet.remove_from_sprite_lists()
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = MOVEMENT_SPEED
 
+
+
+    def on_key_release(self, key, modifiers):
+
+        """Called when the user releases a key. """
+
+        # If a player releases a key, zero out the speed.
+        # This doesn't work well if multiple keys are pressed.
+        # Use 'better move by keyboard' example if you need to
+        # handle this.
+
+        if key == arcade.key.W or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+
+        elif key == arcade.key.A or key == arcade.key.D:
+            self.player_sprite.change_x = 0
 
 def main():
-    game = MyGame()
-    game.setup()
+    """ Main function """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
     arcade.run()
 
 
